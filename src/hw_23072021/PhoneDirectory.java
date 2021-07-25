cd4aa57ba4d2093a7eee287748ed6d141d42e952
pavel <zzzzfogzzzz@gmail.com>
2021-07-25 15:27:01 +0600

    hw_3

---
+++ src/hw_23072021/PhoneDirectory.java
@@ -0,0 +1,18 @@
+package hw_23072021;
+
+import java.util.*;
+
+public class PhoneDirectory {
+    private static final Map<String, List<String>> phoneDirectory = new TreeMap<>();
+
+    static void add(String surname, String... phoneNumbers) {
+        List<String> phones = new ArrayList<>(Arrays.asList(phoneNumbers));
+        for (int i = 0; i < phones.size(); i++) {
+            phoneDirectory.put(surname, phones);
+        }
+    }
+
+    static List<String> get(String surname) {
+        return phoneDirectory.getOrDefault(surname, new ArrayList<>(Arrays.asList("no numbers")));
+    }
+}
