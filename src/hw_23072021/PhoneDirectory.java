9e68d788398b32226ab4b564640540c87fc9f390
pavel <zzzzfogzzzz@gmail.com>
2021-07-27 21:18:10 +0600

    hw_3.2

--- src/hw_23072021/PhoneDirectory.java
+++ src/hw_23072021/PhoneDirectory.java
@@ -3,14 +3,14 @@ package hw_23072021;
 import java.util.*;
 
 public class PhoneDirectory {
-    private static final Map<String, List<String>> phoneDirectory = new TreeMap<>();
+    private static final Map<String, Set<String>> phoneDirectory = new TreeMap<>();
 
     static void add(String surname, String... phoneNumbers) {
-        List<String> phones = new ArrayList<>(Arrays.asList(phoneNumbers));
+        Set<String> phones = new TreeSet<>(Arrays.asList(phoneNumbers));
         phoneDirectory.put(surname, phones);
     }
 
-    static List<String> get(String surname) {
-        return phoneDirectory.getOrDefault(surname, new ArrayList<>(Arrays.asList("no numbers")));
+    static Set<String> get(String surname) {
+        return phoneDirectory.getOrDefault(surname, new TreeSet<>(Arrays.asList("no numbers")));
     }
 }
